e37934c07c31d592a1ffba956e222559e2e5c67e
jeropulga <pulgarinjeronimo411@gmail.com>
2026-09-24 16:43:00 -0500

    refactor: Organizo y subo por fin los cambios de la Rama Develop

---
+++ src/routes/v1/progress.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getProgress,
+    getProgressById,
+    createProgress,
+    updateProgress,
+    patchProgress,
+    deleteProgress
+} = require('../../controllers/progress.controller');
+
+router.get('/', getProgress);
+router.get('/:id', getProgressById);
+router.post('/', createProgress);
+router.put('/:id', updateProgress);
+router.patch('/:id', patchProgress);
+router.delete('/:id', deleteProgress);
+
+module.exports = router;
---
+++ src/routes/v1/workouts.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getWorkouts,
+    getWorkoutById,
+    createWorkout,
+    updateWorkout,
+    patchWorkout,
+    deleteWorkout
+} = require('../../controllers/workouts.controller');
+
+router.get('/', getWorkouts);
+router.get('/:id', getWorkoutById);
+router.post('/', createWorkout);
+router.put('/:id', updateWorkout);
+router.patch('/:id', patchWorkout);
+router.delete('/:id', deleteWorkout);
+
+module.exports = router;
